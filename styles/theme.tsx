cbaa0a542a41e83ee61e36b9c6e521cc87948939
Sandeep.M <sandeep.m24.rathnam@gmail.com>
2022-04-02 23:57:27 +0530

    Changed Theme

--- styles/theme.tsx
+++ styles/theme.tsx
@@ -1,68 +1,17 @@
-import { extendTheme } from '@chakra-ui/react';
+import { extendTheme } from "@chakra-ui/react";
+import { mode } from "@chakra-ui/theme-tools";
 
 const theme = extendTheme({
     styles: {
-        global: {
+        global: (props: any) => ({
             body: {
-                fontFamily: 'body',
-                color: 'black',
+                bg: mode("#f8f9fa", "#121212")(props),
             },
-        },
+        }),
     },
-    /*CHANGE COLORS*/
-    colors: {
-        black: '#111827',
-        white: '#F9FAFB',
-        grey: '#D1D5DB',
-        red: '#9D174D',
-    },
-    /*CHANGE FONTS*/
-    fonts: {
-        heading: `'Times New Roman', sans-serif`,
-        body: `'Helvetica', sans-serif`,
-    },
-    components: {
-        Button: {
-            baseStyle: {
-                fontWeight: '600',
-                textTransform: 'uppercase',
-                borderRadius: 'md',
-            },
-            sizes: {
-                md: {
-                    fontSize: 'md',
-                    px: 6,
-                    py: 4,
-                },
-                lg: {
-                    fontSize: 'lg',
-                    px: 8,
-                    py: 6,
-                },
-            },
-            variants: {
-                outline: {
-                    border: '3px solid',
-                    borderColor: 'red',
-                    color: 'red',
-                    _hover: {
-                        bg: 'red',
-                        color: 'white',
-                    },
-                },
-                solid: {
-                    bg: 'red',
-                    color: 'white',
-                    _hover: {
-                        bg: 'red',
-                    },
-                },
-            },
-            defaultProps: {
-                size: 'md',
-                variant: 'outline',
-            },
-        },
+    config: {
+        initialColorMode: "dark",
+        useSystemColorMode: false,
     },
 });
 
