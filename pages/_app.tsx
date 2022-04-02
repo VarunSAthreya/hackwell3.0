75d5f4e746d70c82700a1599586c81f2c9cf94b6
Sandeep.M <sandeep.m24.rathnam@gmail.com>
2022-04-03 01:38:43 +0530

    Added Navigation and Footer

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,15 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import { AppProps } from 'next/app';
 import theme from '../styles/theme';
+import Navigation from '../components/Navigation/Navigation';
+import Footer from '../components/Footer/Footer';
 
 const App = ({ Component, pageProps }: AppProps) => {
     return (
         <ChakraProvider theme={theme}>
-            <Component {...pageProps} />
+            <Navigation />
+               <Component {...pageProps} />
+            <Footer />
         </ChakraProvider>
     );
 };
