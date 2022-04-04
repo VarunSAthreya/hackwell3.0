3fcaca34af508bed73ebe3471fc528ced286ebed
Sandeep.M <sandeep.m24.rathnam@gmail.com>
2022-04-04 23:34:42 +0530

    Made Register Page

---
+++ pages/register.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { NextPage } from 'next';
+import { Text } from '@chakra-ui/react';
+
+const Register: NextPage = () => {
+    return (
+        <Text>Register form</Text>
+    )
+}
+
+export default Register;
