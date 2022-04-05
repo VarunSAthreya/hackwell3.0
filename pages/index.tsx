626b1fd8e421f070ca6afc59a0c7bab1e50a86ee
Sandeep.M <sandeep.m24.rathnam@gmail.com>
2022-04-06 00:48:14 +0530

    Added Content

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,74 @@
-import { Text } from '@chakra-ui/react';
+import {
+  Box,
+  Container,
+  Divider,
+  SimpleGrid,
+  Image,
+  Flex,
+  Heading,
+  Text,
+  Stack,
+  StackDivider,
+  HStack,
+  useColorModeValue
+} from '@chakra-ui/react';
 import { NextPage } from 'next';
 import React from 'react';
+import Canvas from '../components/UI/Canvas/Canvas';
+
 
 const Home: NextPage = () => {
-    return <Text>Home</Text>;
+  return (
+    <React.Fragment>
+      <>
+        <Canvas />
+        <Stack spacing={32} pos={'fixed'} top={'30%'} left={'15%'}>
+          <Stack spacing={3}>
+            <Text
+              textTransform={'uppercase'}
+              color={'red.500'}
+              fontWeight={600}
+              fontSize={'sm'}
+              bg={'white'}
+              p={2}
+              alignSelf={'flex-start'}
+              rounded={'md'}>
+              JSSATE-B PRESENTS
+            </Text>
+            <Heading fontSize={'6xl'} letterSpacing={4}>HACKWELL 3.0</Heading>
+            <Text color={'gray.500'} fontSize={'lg'} letterSpacing={3}>
+              Every Line Matters
+            </Text>
+          </Stack>
+          <HStack
+            spacing={8}
+            divider={
+              <StackDivider
+                borderColor={'whiteAlpha.500'}
+              />
+            }>
+            <Box>
+              <Heading>22</Heading>
+              <Text color={'gray.500'} fontSize={'lg'}>DAYS</Text>
+            </Box>
+            <Box>
+              <Heading>22</Heading>
+              <Text color={'gray.500'} fontSize={'lg'}>HRS</Text>
+            </Box>
+            <Box>
+              <Heading>22</Heading>
+              <Text color={'gray.500'} fontSize={'lg'}>MINS</Text>
+            </Box>
+            <Box>
+              <Heading>22</Heading>
+              <Text color={'gray.500'} fontSize={'lg'}>SECS</Text>
+            </Box>
+          </HStack>
+        </Stack>
+      </>
+    </React.Fragment>
+  )
 };
 
 export default Home;
+
