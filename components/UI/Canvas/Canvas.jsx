1f01d8abb6d07b85341525b1ccacbd7321098978
Sandeep.M <sandeep.m24.rathnam@gmail.com>
2022-04-05 23:43:56 +0530

    Made Canvas Component

---
+++ components/UI/Canvas/Canvas.jsx
@@ -0,0 +1,109 @@
+import React, { useEffect } from 'react';
+import { ImageCode } from '../../../assests/ImageCode';
+
+const Canvas = () => {
+    useEffect(() => {
+        const myImage = new Image();
+        myImage.src = ImageCode;
+        myImage.addEventListener('load', function () {
+            const canvas = document.getElementById('canvas1');
+            const ctx = canvas.getContext('2d');
+            canvas.width = 1536;
+            canvas.height = 753;
+
+            ctx.drawImage(myImage, 0, 0, canvas.width, canvas.height);
+            const pixels = ctx.getImageData(0, 0, canvas.width, canvas.height);
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+            let particlesArray = [];
+            const numberOfParticles = 5000;
+
+            let mappedImage = [];
+            for (let y = 0; y < canvas.height; y++) {
+                let row = [];
+                for (let x = 0; x < canvas.width; x++) {
+                    const red = pixels.data[y * 4 * pixels.width + x * 4];
+                    const green =
+                        pixels.data[y * 4 * pixels.width + (x * 4 + 1)];
+                    const blue =
+                        pixels.data[y * 4 * pixels.width + (x * 4 + 2)];
+                    const brightness = calculateRelativeBrightness(
+                        red,
+                        green,
+                        blue
+                    );
+                    const cell = [brightness];
+                    row.push(cell);
+                }
+                mappedImage.push(row);
+            }
+            // console.log(mappedImage);
+
+            function calculateRelativeBrightness(red, green, blue) {
+                return (
+                    Math.sqrt(red + red + (green + green) + (blue + blue)) / 55
+                );
+            }
+
+            class Particle {
+                constructor() {
+                    this.x = Math.random() * canvas.width;
+                    this.y = 0;
+                    this.speed = 0;
+                    this.velocity = Math.random() * 3.0;
+                    this.size = Math.random() * 2.5 + 1;
+                    this.position1 = Math.floor(this.y);
+                    this.position2 = Math.floor(this.x);
+                }
+                update() {
+                    this.position1 = Math.floor(this.y);
+                    this.position2 = Math.floor(this.x);
+                    this.speed = mappedImage[this.position1][this.position2][0];
+                    let movement = 2.5 - this.speed + this.velocity;
+                    this.y += movement;
+                    if (this.y >= canvas.height) {
+                        this.y = 0;
+                        this.x = Math.random() * canvas.width;
+                    }
+                }
+                draw() {
+                    ctx.beginPath();
+                    ctx.fillStyle = 'red';
+                    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+                    ctx.fill();
+                }
+            }
+            function init() {
+                for (let i = 0; i < numberOfParticles; i++) {
+                    particlesArray.push(new Particle());
+                }
+            }
+            init();
+            function animate() {
+                ctx.globalAlpha = 0.05;
+                ctx.fillStyle = 'rgb(0, 0, 0)';
+                ctx.fillRect(0, 0, canvas.width, canvas.height);
+                ctx.globalAlpha = 0.02;
+                for (let i = 0; i < particlesArray.length; i++) {
+                    particlesArray[i].update();
+                    ctx.globalAlpha = particlesArray[i].speed * 0.5;
+                    particlesArray[i].draw();
+                }
+                requestAnimationFrame(animate);
+            }
+            animate();
+        });
+    }, []);
+
+    return (
+        <div
+            style={{
+                textAlign: 'center',
+            }}
+        >
+            <canvas id="canvas1" width={'100%'} height={'750px'}></canvas>
+        </div>
+    );
+};
+
+export default Canvas;
