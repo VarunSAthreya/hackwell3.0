0a2f390d67a57b77a5085d4642072c65490622c8
Sandeep.M <sandeep.m24.rathnam@gmail.com>
2022-04-09 19:26:51 +0530

    Provided Proper Links

--- components/Navigation/NavLinks/NavLinks.tsx
+++ components/Navigation/NavLinks/NavLinks.tsx
@@ -1,7 +1,6 @@
 import { FC, ReactNode } from 'react';
 import { Link, keyframes, usePrefersReducedMotion } from '@chakra-ui/react';
 
-
 const fill = keyframes`
 0% {
     width: 0%;
@@ -14,13 +13,16 @@ const fill = keyframes`
   100% {
     width: 100%;
     height: 100%;
-    background: #333;
+    background: #CC01FF;
   }
-`
+`;
 
 const NavLink: FC = ({ children }: { children: ReactNode }) => {
-    const prefersReducedMotion = usePrefersReducedMotion()
-    const animation = prefersReducedMotion ? undefined : `${fill} 0.2s forwards`
+    const prefersReducedMotion = usePrefersReducedMotion();
+    const animation = prefersReducedMotion
+        ? undefined
+        : `${fill} 0.2s forwards`;
+    console.log(children);
     return (
         <Link
             px={2}
@@ -28,39 +30,40 @@ const NavLink: FC = ({ children }: { children: ReactNode }) => {
             _hover={{
                 textDecoration: 'none',
                 color: '#fff',
-                zIndex: 1
+                zIndex: 1,
             }}
             _focus={{ outline: 'none' }}
             sx={{
-                position: "relative",
-                letterSpacing: "2px",
-                padding: "12px",
+                position: 'relative',
+                letterSpacing: '2px',
+                padding: '12px',
                 transition: 'all',
-                ":after": {
+                ':after': {
                     content: '"."',
                     textAlign: 'left',
                     margin: 'auto',
                     opacity: 0,
                     rounded: 4,
-                    position: "absolute",
+                    position: 'absolute',
                     bottom: 0,
                     right: 0,
                     left: 0,
                     width: '0%',
                     color: 'transparent',
-                    height: "2px",
-                    background: "#aaa",
+                    height: '2px',
+                    background: '#aaa',
                 },
-                ":hover::after": {
+                ':hover::after': {
                     zIndex: -10,
                     animation: animation,
                     opacity: 1,
                 },
             }}
-            href={children.toString().toLowerCase()}>
+            href={'#' + children.toString().toLowerCase()}
+        >
             {children}
         </Link>
     );
-}
+};
 
 export default NavLink;
